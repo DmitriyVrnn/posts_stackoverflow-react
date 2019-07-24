d23e9fea91d17fb9a3eb9d14ba4807f48cf1b0d3
Dmitriy <kaishien777@gmail.com>
2019-07-25 00:43:21 +0300

    reafactoring

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import './App.css';
 
 import Posts from './components/Posts';
 
 function App() {
   return (
-    <main className="App">
-      <Posts />
-    </main>
+    <Posts />
   );
 }
 
